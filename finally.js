67ccc471353abcfe47cc900809c5d8f87a9f519e
Matthew Andrews <matt@mattandre.ws>
2014-10-11 12:27:45 +0100

    Wrap polyfill so that if it's used direct it doesn't leak

--- finally.js
+++ finally.js
@@ -5,27 +5,29 @@
  * @author @stefanpenner, @matthew-andrews
  */
 
-// Get a handle on the global object
-var local;
-if (typeof global !== 'undefined') local = global;
-else if (typeof window !== 'undefined' && window.document) local = window;
-else local = self;
+(function() {
+	// Get a handle on the global object
+	var local;
+	if (typeof global !== 'undefined') local = global;
+	else if (typeof window !== 'undefined' && window.document) local = window;
+	else local = self;
 
-// Polyfill (verb)
-var finallySupport = "finally" in local.Promise.prototype;
-if (!finallySupport) local.Promise.prototype['finally'] = finallyPolyfill;
+	// Polyfill (verb)
+	var finallySupport = "finally" in local.Promise.prototype;
+	if (!finallySupport) local.Promise.prototype['finally'] = finallyPolyfill;
 
-// Polyfill (noun)
-function finallyPolyfill(callback) {
-	var constructor = this.constructor;
+	// Polyfill (noun)
+	function finallyPolyfill(callback) {
+		var constructor = this.constructor;
 
-	return this.then(function(value) {
-			return constructor.resolve(callback()).then(function() {
-				return value;
+		return this.then(function(value) {
+				return constructor.resolve(callback()).then(function() {
+					return value;
+				});
+			}, function(reason) {
+				return constructor.resolve(callback()).then(function() {
+					throw reason;
+				});
 			});
-		}, function(reason) {
-			return constructor.resolve(callback()).then(function() {
-				throw reason;
-			});
-		});
-}
+	}
+}());
